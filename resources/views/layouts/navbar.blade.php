e8e68aae8880aafb4545c444c6985c4720841237
Samuel Mbugua <smbugua@STANDARDMEDIA.CO.KE>
2020-06-01 02:19:11 +0300

    Logo added, and links on index page and color

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{asset('img/backlink_logo.png')}}" alt="ogo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link calltoaction" href="#">Submit your domain</a>
+                    </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
