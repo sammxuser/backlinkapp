650bec5b5cdbe26f6bff43e63e50ee21b432ed4a
Samuel Mbugua <smbugua@STANDARDMEDIA.CO.KE>
2020-06-02 13:54:43 +0300

    Added images on the index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-6 d-flex flex-column justify-content-center">
+        <div class="py-3"><h1>Link buildubg is one of the most important thing in <span class="textmaincolor">SEO</span></h1></div>
+        <div><h4>Every little helps. Submit your domain and get do follow backlink from our website</h4> </div>
+        <div class="py-5"><a href="#" class="btn btn-primary">Submit your domain</a></div>
+        </div>
+        <div class="col-6">
+        <div class="d-flex justify-content-end align-items-center">
+            <img class=" py-5 pl-5" src="{{asset('img/backlink.png')}}" alt="Image of the main page">
+        </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <h2 class="font-weight-bolder">Categories</h2>
+    </div>
+</div>
 @endsection
