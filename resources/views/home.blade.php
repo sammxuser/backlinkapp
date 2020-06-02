c227ea0e1f4f95326001cf7d88900154813988da
Samuel Mbugua <smbugua@STANDARDMEDIA.CO.KE>
2020-06-03 01:42:32 +0300

    Model and controller for categories

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header d-flex align-items-center justify-content-between">
+                <div class="inline-block">Dashboard</div>
+                <div class="inline-block">
+                    <form method="POST" action="{{route('logout')}}">
+                    @csrf    
+                    <button type="submit">Logout</button>
+                    </form>
+                </div>    
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
+                    <div class="d-flex align-items-between">
+                        <a class="btn btn-success w-50 m-1" href="{{route('category.create')}}">Add new category</a>
+                    </div>
                     You are logged in!
                 </div>
             </div>
